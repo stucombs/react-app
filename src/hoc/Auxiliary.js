fbce900072d3f061d389a406846abe7538498162
stucombs <stuart.combs@bcforward.com>
2020-02-21 13:41:52 -0500

    Archive project 1

--- src/hoc/Auxiliary.js
+++
@@ -1,6 +0,0 @@
-//On Windows systems, this file must be name "Auxiliary" as Aux is a reserved word on this OS
-import React from 'react';
-
-const aux = props => props.children;
-
-export default aux;
