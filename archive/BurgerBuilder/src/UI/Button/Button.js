88fe70aa817b5bab8151493fe638f8b0753203f0
stucombs <stuart.combs@bcforward.com>
2020-03-05 10:39:16 -0500

    UI fix

--- archive/BurgerBuilder/src/UI/Button/Button.js
+++ archive/BurgerBuilder/src/UI/Button/Button.js
@@ -3,7 +3,7 @@ import React from 'react';
 import classes from './Button.css';
 
 const button = (props) => (
-	<button onClick={props.clicked} className={[classes.Button]}>{props.children}</button>
+	<button onClick={props.clicked} className={[classes.Button, classes[props.btnType]].join(' ')}>{props.children}</button>
 );
 
 export default button;
